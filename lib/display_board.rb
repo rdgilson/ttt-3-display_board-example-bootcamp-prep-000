346c5973a02aaa06c3dcd456087e89faacb8080c
Randy Gilson <randygilson@gmail.com>
2017-10-31 08:52:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -7,3 +7,5 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
